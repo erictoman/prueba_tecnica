be4692efdb133336c96e55bd51c772d6a344b52c
Erictoman <ericto117@gmail.com>
2020-08-25 19:03:03 -0500

    Inicio

---
+++ README.md
@@ -0,0 +1,5 @@
+# Prueba tecnica
+
+## Inicio
+
+    Parte 1: Servidor NodeJS+Express.
---
+++ Servidor/servidor.js
@@ -0,0 +1,14 @@
+//Librerias
+const express = require("express");
+const http = require("http");
+//Express
+const app = express();
+//Express sobre http
+const server = http.createServer(app);
+//Puerto ya sea por el entorno o definido por mi.
+const PORT = process.env.PORT || 1330;
+
+//Lanzar servidor
+server.listen(PORT, () => {
+  console.log(`Servidor en ${PORT}`);
+});
