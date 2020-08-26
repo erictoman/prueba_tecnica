3c98dfe47a6b3bc06c5260975c3944ce2b8c385f
Erictoman <ericto117@gmail.com>
2020-08-25 22:09:56 -0500

    Server listo

--- Servidor/rutas.js
+++ Servidor/rutas.js
@@ -3,12 +3,30 @@ var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
 
+//JWT
+var jwt = require("jsonwebtoken");
+process.env.JWT_SECRET_KEY = "HALO INFINITE";
+var auth = require("express-jwt-token");
+
+//Secreto
+var token_pass = "HALO INFINITE";
+
 //Usuario
 var SchemaUsuario = new mongoose.Schema({
   usuario: String,
   password: String,
 });
 const Usuario = mongoose.model("Usuario", SchemaUsuario);
+//Registro
+var SchemaRegistro = new mongoose.Schema({
+  campo1: String,
+  campo2: String,
+  campo3: String,
+  campo4: String,
+  fecha: Date,
+});
+
+const Registro = mongoose.model("Registro", SchemaRegistro);
 
 //Coneccion a mongodb
 var uri = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb";
@@ -20,26 +38,26 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     throw err;
   }
 );
-//JWT
-var jwt = require("jsonwebtoken");
-//Secreto
-var token_pass = "HALO INFINTE";
 
+//Autenticacion JWT
 router.post("/api/login", (req, res) => {
   var usuarioB = req.body.usuario;
   var passwordB = req.body.password;
   if (usuarioB != undefined && passwordB != undefined) {
-    Usuario.find({ usuario: req.body.usuario }, (error, resbusqueda) => {
-      if (error) {
-        throw error;
+    Usuario.find(
+      { usuario: usuarioB, password: passwordB },
+      (error, resbusqueda) => {
+        if (error) {
+          throw error;
+        }
+        if (resbusqueda.length == 1) {
+          res.send({ token: jwt.sign({ usuario: usuarioB }, token_pass) });
+        } else {
+          res.send({ token: "" });
+          return;
+        }
       }
-      if (resbusqueda.length == 0) {
-        res.send({ token: "" });
-      } else {
-        res.send({ token: "" });
-        return;
-      }
-    });
+    );
   } else {
     res.send({ resultado: "Error" });
   }
@@ -76,10 +94,39 @@ router.post("/api/signup", (req, res) => {
   }
 });
 
-router.post("/api/altaregistro", (req, res) => {});
-router.post("/api/ultimoregistro", (req, res) => {});
-router.get("/api/test", (req, res) => {
-  res.send({ prueba: "ok" });
+router.post("/api/altaregistro", (req, res) => {
+  var c1 = req.body.c1;
+  var c2 = req.body.c2;
+  var c3 = req.body.c3;
+  var c4 = req.body.c4;
+  var c5 = req.body.c5;
+  if (c1 && c2 && c3 && c4 && c5) {
+    var reg = new Registro({
+      campo1: c1,
+      campo2: c2,
+      campo3: c3,
+      campo4: c4,
+      fecha: c5,
+    });
+    Registro.collection.insertOne(reg);
+    res.send({ resultado: "ok" });
+  } else {
+    res.send({ resultado: "campos incompletos" });
+  }
+});
+
+router.get("/api/ultimoregistro", auth.jwtAuthProtected, (req, res) => {
+  if (req.body.usuario == "eric") {
+    Registro.findOne({}, null, { sort: { fecha: -1 } }, function (
+      err,
+      registros
+    ) {
+      if (err) {
+        throw err;
+      }
+      return res.send(registros);
+    });
+  }
 });
 
 module.exports = router;
